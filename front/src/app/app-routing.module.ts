4602024b172e5bfea2ef97d149468b27e875f0f8
Fernando554 <fernando_1320114093@uptecamac.edu.mx>
2024-03-14 00:24:23 -0600

    Primer commit

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Shell } from '@app/shell/shell.service';
+import { CalcularComponent } from './calcular/calcular.component';
+
+const routes: Routes = [
+  { path: 'calcular', component: CalcularComponent },
+  Shell.childRoutes([{ path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule) }]),
+  // Fallback when no prior route is matched
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AppRoutingModule {}
